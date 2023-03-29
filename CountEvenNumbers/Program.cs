ed9bbbee60454e2d4e8930dde56dc0300b3dd44d
Svetlana <khabarova09@rambler.ru>
2023-03-29 09:35:29 +0300

    кол-во положительных на 29марта2023

---
+++ CountEvenNumbers/Program.cs
@@ -0,0 +1,36 @@
+﻿//Ввести с клавиатуры М чисел. Сколько чисел больше 0?
+//{0,7,8,-2,-2} -> 2
+
+
+void FillArray(int[] arr)
+{
+  for (int i = 0; i < arr.Length; i++)
+  {
+    Console.Write("Введите число ");
+    arr[i] = Convert.ToInt32(Console.ReadLine());
+  }
+}
+
+void PrintArray(int[] ar)
+{
+  foreach (int i in ar)
+  {
+    Console.Write($"{i},");
+  }
+  Console.WriteLine();
+}
+
+int CountEven(int[] array)
+{
+  int count = 0;
+  for (int i = 0; i < array.Length; i++)
+  if(array[i]>0) count=count+1;
+  return count;
+}
+
+
+int[] array = new int[4];
+Console.WriteLine(array.Length);
+FillArray(array);
+PrintArray(array);
+Console.WriteLine(CountEven(array));
